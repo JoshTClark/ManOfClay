65ee9c7ddbb3316c1ceaee228739c1d007ab79b9
Abe Jeyapratap <39566813+abe1907@users.noreply.github.com>
2018-05-24 13:36:38 -0400

    Update Enemy.java

--- src/MainPackages/Enemy.java
+++ src/MainPackages/Enemy.java
@@ -1,15 +1,16 @@
 package MainPackages;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author Josh
+ * This class makes an Enemy object that engages in combat with the Player and attempts to prevent the
+ * Player from being victorious.
+ * 
+ * @author (Abe) 
  */
-public class Enemy {
-    
+public class Enemy extends Interactable {
+    /**
+     * Constructor for objects of class Enemy
+     */
+    public Enemy(int x, int y) {
+        super(x, y, 32, 32, 10, 10, 50, new Sprite("TestSprite.png", 3, 32, 32, 64, 64, 4));
+    }
 }
