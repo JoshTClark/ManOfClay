320a95ddac58f9e2dfffc0ea857db979800bde51
Abe Jeyapratap <39566813+abe1907@users.noreply.github.com>
2018-05-29 13:39:14 -0400

    Update Level.java

--- src/MainPackages/Level.java
+++ src/MainPackages/Level.java
@@ -1,5 +1,39 @@
 package MainPackages;
 
+import java.awt.Graphics;
+import java.awt.event.*;
+import java.awt.event.KeyEvent;
+import java.util.logging.Logger;
+import javax.swing.*;
+import java.lang.Object;
+
+/**
+ * Has everything in a level; can be saved to a file.
+ * 
+ * @author (Abe) 
+ */
 public class Level {
+    private LevelGen lev;
+    public enum LevelGen {EASY, HARD}
     
+    public Level (String currentDifficulty) {
+        if ("hard".equals(currentDifficulty.toLowerCase())) 
+            lev = LevelGen.HARD;
+        else
+            lev = LevelGen.EASY;
+    }
+
+    public void loadLevel(int numLevel) {
+        switch (lev) {
+            case HARD:
+                clearCurrentLevel();
+            default:
+                clearCurrentLevel()
+            
+        }
+    }
+
+    private void clearCurrentLevel() {
+        
+    }
 }
